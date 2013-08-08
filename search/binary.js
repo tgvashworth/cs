7f5fa3fdd9a891c10082093fd004f06a5d61c837
Tom Ashworth <tom@phuu.net>
2013-08-08 23:02:30 +0100

    Smarterise the binary search.

--- search/binary.js
+++ search/binary.js
@@ -7,18 +7,21 @@ var assert = require('assert');
  * It recurses into itself with every-smaller halves of the original array until
  * it has an empty array or the value it needs.
  */
-var search = function search(arr, val) {
-  var pivot = Math.floor(arr.length / 2),
-      pivotVal = arr[pivot];
-  if (!arr.length) { return null; }
-  if (val === pivotVal) { return pivot; }
-  if (val < pivotVal) { return search(arr.slice(0, pivot), val); }
-  if (val > pivotVal) {
-    var res = search(arr.slice(pivot + 1), val);
-    if (res === null) { return null; }
-    return pivot + 1 + res;
+var search = function search(arr, val, min, max) {
+  if (!min) { min = 0; }
+  if (!max) { max = arr.length; }
+  if (max - min === 0) { return null; }
+
+  var mid = min + Math.floor((max - min) / 2),
+      midVal = arr[mid];
+
+  if (val < midVal) {
+    return search(arr, val, min, mid);
   }
-  return null;
+  if (val > midVal) {
+    return search(arr, val, mid + 1, max);
+  }
+  return mid;
 };
 
 assert.deepEqual(search([1,2,3,4], 1), 0);
@@ -26,4 +29,6 @@ assert.deepEqual(search([1,2,3,4], 2), 1);
 assert.deepEqual(search([1,2,3,4], 4), 3);
 assert.deepEqual(search([1,2,3,4,5,6], 4), 3);
 assert.deepEqual(search([1,2,3,4], 5), null);
+assert.deepEqual(search([1,3,4,10,25,90], 5), null);
+assert.deepEqual(search([1,3,4,10,25,90], 25), 4);
 assert.deepEqual(search([], 1), null);
