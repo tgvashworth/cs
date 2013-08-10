3a298dc8b5588c75da6ed7ad45ee0ba21b2785be
Tom Ashworth <tom@phuu.net>
2013-08-10 12:58:48 +0100

    Swap head and tail, and support queue-like ops on linked list.

--- data-structures/linked-list.js
+++ data-structures/linked-list.js
@@ -3,7 +3,8 @@ var assert = require('assert');
 /**
  * Linked-list.
  *
- * Supports push, pop, at, rm, last, first and length, plus traverse for logging.
+ * Supports push, pop, unshift, shift, enqueue, dequeue, at, rm, last, first
+ * and length, plus traverse for logging.
  */
 var linkedList = (function () {
   var linkedList = {};
@@ -14,44 +15,65 @@ var linkedList = (function () {
   linkedList.push = function (val) {
     var newNode = {};
     newNode.value = val;
-    if (this.tail) { this.tail.next = newNode; }
-    if (!this.head) { this.head = newNode; }
-    this.tail = newNode;
+    if (this.head) { this.head.next = newNode; }
+    if (!this.tail) { this.tail = newNode; }
+    this.head = newNode;
+    return this;
   };
 
   /**
-   * Pop the last node off the end
+   * Pop the last node off the end. Dequeue.
    */
-  linkedList.pop = function () {
-    if (!this.tail) { return null; }
+  linkedList.pop = linkedList.dequeue = function () {
+    if (!this.head) { return null; }
     var len = this.length();
     return this.rm(len - 1);
+  };
+
+  /**
+   * Add an element to the start (tail) of the list. Enqueue.
+   */
+  linkedList.unshift = linkedList.enqueue = function (val) {
+    if (!this.tail) { return this.push(val); }
+    var newNode = {};
+    newNode.value = val;
+    newNode.next = this.tail;
+    this.tail = newNode;
+    return this;
+  };
+
+  /**
+   * Remove an element from the start (tail) of the list.
+   */
+  linkedList.shift = function () {
+    if (!this.tail) { return null; }
+    return this.rm(0);
   };
 
   /**
    * Grab the first value from the list
    */
   linkedList.first = function (val) {
-    if (!this.head) { return null; }
-    return this.head.value;
+    if (!this.tail) { return null; }
+    return this.tail.value;
   };
 
   /**
    * Grab the last value from the list
    */
   linkedList.last = function (val) {
-    if (!this.tail) { return null; }
-    return this.tail.value;
+    if (!this.head) { return null; }
+    return this.head.value;
   };
 
   /**
    * Grab a value out from the list
    */
   linkedList.at = function (index) {
-    if (!this.tail) { this.tail = this.head; }
-    if (!this.head) { return null; }
+    if (!this.head) { this.head = this.tail; }
+    if (!this.tail) { return null; }
     var visited = 0,
-        current = this.head;
+        current = this.tail;
     while (current && visited < index) {
       current = current.next;
       visited++;
@@ -63,9 +85,9 @@ var linkedList = (function () {
    * Remove a node from the list at index
    */
   linkedList.rm = function (index) {
-    if (!this.head) { return null; }
+    if (!this.tail) { return null; }
     var visited = 0,
-        current = this.head,
+        current = this.tail,
         last = null;
     while (current && visited < index) {
       last = current;
@@ -73,8 +95,8 @@ var linkedList = (function () {
       visited++;
     }
     if (last) { last.next = current.next; }
-    if (this.head === current) { this.head = current.next; }
-    if (this.tail === current) { this.tail = last; }
+    if (this.tail === current) { this.tail = current.next; }
+    if (this.head === current) { this.head = last; }
     return current.value;
   };
 
@@ -82,9 +104,9 @@ var linkedList = (function () {
    * Get the list's length
    */
   linkedList.length = function () {
-    if (!this.head) { return 0; }
+    if (!this.tail) { return 0; }
     var visited = 0,
-        current = this.head;
+        current = this.tail;
     while (current.next) {
       current = current.next;
       visited++;
@@ -97,14 +119,15 @@ var linkedList = (function () {
    * TODO get rid of the duplication here
    */
   linkedList.traverse = function () {
-    if (!this.head) { return; }
+    if (!this.tail) { return; }
     var visited = 0,
-        current = this.head;
+        current = this.tail;
     while (current.next) {
       console.log('%d: %d', visited++, current.value);
       current = current.next;
     }
     console.log('%d: %d', visited++, current.value);
+    return this;
   };
 
   return linkedList;
@@ -153,6 +176,14 @@ if (require.main === module) {
 
   ll.push(1);
   assert.deepEqual(ll.last(), 1);
+  assert.deepEqual(ll.length(), 1);
+
+  ll.unshift(2);
+
+  assert.deepEqual(ll.first(), 2);
+  assert.deepEqual(ll.length(), 2);
+
+  assert.deepEqual(ll.shift(), 2);
   assert.deepEqual(ll.length(), 1);
 
   console.log('done');
